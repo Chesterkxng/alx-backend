b3c537dd177e1c6c0c442a0d20ef30a0df2a575a
Chesterkxng <cgoita00@gmail.com>
2024-05-17 12:06:17 +0000

    pystyle fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from typing import Tuple
 """
 Helper Function
 """
+from typing import Tuple
 
 
-def index_range(page : int, page_size : int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """
     return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes
