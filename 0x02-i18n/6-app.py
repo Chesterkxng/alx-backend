c2a6ef08d961804a4a7783afd452f2585cfecc21
Chesterkxng <cgoita00@gmail.com>
2024-05-28 19:30:46 +0000

    get local update

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+
+# Config class
+class Config:
+    """
+    configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+# Use Config to set Babel's default locale and timezone
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determine the best match with our supported languages
+    """
+    lang = request.args.get("locale")
+    if lang and lang in Config.LANGUAGES:
+        return lang
+    if g.user and g.user.locale in Config.LANGUAGES:
+        return g.user.locale
+    header_lang = request.accept_languages.best_match(Config.LANGUAGES)
+    if header_lang:
+        return header_lang
+    return Config.BABEL_DEFAULT_LOCALE
+
+
+@app.route("/")
+def index():
+    """
+    Renders index.html template
+    """
+    return render_template("6-index.html", user=g.user)
+
+
+def get_user():
+    """
+    returns the user if exists
+    """
+    id = request.args.get('login_as')
+    if id and int(id) in users.keys():
+        return users[int(id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    use get_user to find and  set it as a global on flask.g.user
+    """
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
